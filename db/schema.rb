bb3516ecd334509fede8e5baabdac010111c2392
Rob Ingram <rob@headcrash.org>
2022-08-13 09:14:19 +1200

    Add user association to listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_210335) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_211221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_210335) do
     t.date "available_from"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_210335) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "listings", "users"
 end
