edff4aef51cf40ed68ecbda7c44503cfc272d72c
Rob Ingram <rob@headcrash.org>
2022-08-14 11:27:42 +1200

    Add enquiry update and delete to turbo stream

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,4 +2,7 @@ class Enquiry < ApplicationRecord
   belongs_to :listing
 
   after_create_commit -> { broadcast_prepend_to "enquiries", partial: "enquiries/enquiry_item", locals: { enquiry: self }, target: "enquiries" }
+  after_update_commit -> { broadcast_replace_to "enquiries", partial: "enquiries/enquiry_item" }
+  after_destroy_commit -> { broadcast_remove_to "enquiries" }
+  # broadcasts_to ->(enquiry) { "enquiries" }, partial: "enquiries/enquiry_item", inserts_by: :prepend
 end
