ef6dece125d68dcd2e3cc0a84feb169bfca0bfd1
Rob Ingram <rob@headcrash.org>
2022-08-14 11:18:11 +1200

    Use turbo stream for new enquiries

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,3 +1,5 @@
 class Enquiry < ApplicationRecord
   belongs_to :listing
+
+  after_create_commit -> { broadcast_prepend_to "enquiries", partial: "enquiries/enquiry_item", locals: { enquiry: self }, target: "enquiries" }
 end
